e28e7ecab93a6c5000797193d49c55ab22dd2632
RealDoggoHitman <lahtinenvili1@gmail.com>
2025-03-05 18:21:07 +0200

    spinning rectangle

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
-C:\VulkanSDK\1.4.304.0\Bin\glslc.exe shaderv.vert -o vert.spv
-C:\VulkanSDK\1.4.304.0\Bin\glslc.exe shaderf.frag -o frag.spv
+C:\Users\vilil\source\repos\Green-engine\Libraries\VulkanSDK\1.4.304.0\glslc.exe shaderv.vert -o vert.spv
+C:\Users\vilil\source\repos\Green-engine\Libraries\VulkanSDK\1.4.304.0\glslc.exe shaderf.frag -o frag.spv
